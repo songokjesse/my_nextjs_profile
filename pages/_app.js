c27960c180133390b22a888ab5fde57e1b545e0b
codelab <jessesongok@gmail.com>
2021-09-22 12:54:39 +0300

    added dark theme switching

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
 import 'tailwindcss/tailwind.css'
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
+import {ThemeProvider} from 'next-themes'
 
 function MyApp({ Component, pageProps }) {
   return (
       <>
-        <Navigation/>
-            <Component {...pageProps} />
-        <Footer/>
+          <ThemeProvider attribute="class" enableSystem={false}>
+            <Navigation/>
+                <Component {...pageProps} />
+            <Footer/>
+          </ThemeProvider>
       </>
   )
 }
