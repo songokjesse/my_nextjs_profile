2cf5787389a52986bcaa1a2ba52bf39e0fb63406
codelab <jessesongok@gmail.com>
2021-09-22 11:29:21 +0300

    Initial Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import 'tailwindcss/tailwind.css'
+import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <>
+        <Navigation/>
+            <Component {...pageProps} />
+        <Footer/>
+      </>
+  )
 }
 
 export default MyApp
